f0569afe1db089c76da655d1cf3abc882caf52fc
Daniel Hasselbach <haizz94@icloud.com>
2023-09-21 10:24:09 +0200

    Prefixed all metrics

--- exporter.py
+++ exporter.py
@@ -14,7 +14,7 @@ class GruenbeckCollector(object):
     def collect(self):          
         self.__gbApi.updateInfos()
 
-        next_regeneration = GaugeMetricFamily("next_regeneration", "Timestamp for next regeneration")
+        next_regeneration = GaugeMetricFamily("gruenbeck_next_regeneration", "Timestamp for next regeneration")
         try:
             datetime_object = datetime.strptime(self.__gbApi.nextRegeneration, '%Y-%m-%dT%H:%M:%S')
             next_regeneration.add_metric([], datetime_object.timestamp() * 1000)
@@ -23,77 +23,83 @@ class GruenbeckCollector(object):
 
         yield next_regeneration
 
-        raw_water = GaugeMetricFamily("raw_water", "Raw water hardness")
+        raw_water = GaugeMetricFamily("gruenbeck_raw_water", "Raw water hardness")
         value = float(self.__gbApi.rawWaterHardness)
         raw_water.add_metric([], value)
         yield raw_water
 
         # Output water hardness
         # softWater
-        soft_water = GaugeMetricFamily("soft_water", "Soft water hardness")
+        soft_water = GaugeMetricFamily("gruenbeck_soft_water", "Soft water hardness")
         value = float(self.__gbApi.softWaterHardness)
         soft_water.add_metric([], value)
         yield soft_water
 
         # mode
         # 1=Eco, 2=Comfort, 3=Power
-        mode = GaugeMetricFamily("mode", "Current mode (1=Eco, 2=Comfort, 3=Power)")
+        mode = GaugeMetricFamily("gruenbeck_mode", "Current mode (1=Eco, 2=Comfort, 3=Power)")
         value = int(self.__gbApi.mode)
         mode.add_metric([], value)
         yield mode
 
         # Has error
         # hasError
-        hasError = GaugeMetricFamily("has_error", "Water softener has error")
+        hasError = GaugeMetricFamily("gruenbeck_has_error", "Water softener has error")
         value = 1 if self.__gbApi.hasError else 0
         hasError.add_metric([], value)
         yield hasError
 
         ### Stream
         # mcountwater1
-        water_usage = GaugeMetricFamily("water_usage", "Soft water usage of all time in liters", labels=['tank'])
+        water_usage = GaugeMetricFamily("gruenbeck_water_usage", "Soft water usage of all time in liters", labels=['tank'])
         for idx, x in enumerate(self.__gbApi.waterUsages):
             water_usage.add_metric([str(idx+1)], x)
         yield water_usage
 
         # mcountreg
-        reg_counter = GaugeMetricFamily("reg_counter", "Regeneration counter")
+        reg_counter = GaugeMetricFamily("gruenbeck_reg_counter", "Regeneration counter")
         reg_counter.add_metric([], self.__gbApi.regenerationCounter)
         yield reg_counter
 
         # mflow1
-        water_flow = GaugeMetricFamily("water_flow", "Actual water flow in m3/h", labels=['tank'])
+        water_flow = GaugeMetricFamily("gruenbeck_water_flow", "Actual water flow in m3/h", labels=['tank'])
         for idx, x in enumerate(self.__gbApi.waterFlows):
             water_flow.add_metric([str(idx+1)], x)
         yield water_flow
 
         # mrescapa1
-        remaining_cap_m3 = GaugeMetricFamily("remaining_cap_m3", "Remaining capacity in m3", labels=['tank'])
+        remaining_cap_m3 = GaugeMetricFamily("gruenbeck_remaining_cap_m3", "Remaining capacity in m3", labels=['tank'])
         for idx, x in enumerate(self.__gbApi.remainingCapacitiesM3):
             remaining_cap_m3.add_metric([str(idx+1)], x)
         yield remaining_cap_m3
 
         # residcap1
-        remaining_cap_percent = GaugeMetricFamily("remaining_cap_percent", "Remaining capacity in %", labels=['tank'])
+        remaining_cap_percent = GaugeMetricFamily("gruenbeck_remaining_cap_percent", "Remaining capacity in %", labels=['tank'])
         for idx, x in enumerate(self.__gbApi.remainingCapacitiesPercent):
             remaining_cap_percent.add_metric([str(idx+1)], x)
         yield remaining_cap_percent
 
         # msaltrange
-        salt_range = GaugeMetricFamily("salt_range", "Salt range in days")
+        salt_range = GaugeMetricFamily("gruenbeck_salt_range", "Salt range in days")
         salt_range.add_metric([], self.__gbApi.saltRange)
         yield salt_range
 
         # msaltusage
-        salt_usage = GaugeMetricFamily("salt_usage", "Salt usage in kg")
+        salt_usage = GaugeMetricFamily("gruenbeck_salt_usage", "Salt usage in kg")
         salt_usage.add_metric([], self.__gbApi.saltUsage)
         yield salt_usage
 
         # Maintenance left days
         # mmaint
-        maint_left = GaugeMetricFamily("maint_left", "Left days until next maintenance")
+        maint_left = GaugeMetricFamily("gruenbeck_maint_left", "Left days until next maintenance")
         maint_left.add_metric([], self.__gbApi.maintenanceLeftDays)
         yield maint_left
+
+        # Regeneration status
+        # regstatus
+        reg_status = GaugeMetricFamily("gruenbeck_reg_status", "Regeneration status")
+        reg_status.add_metric([], self.__gbApi.regenerationStatus)
+        yield reg_status
     
 if __name__ == "__main__":
     port = os.getenv("EXPORTER_PORT")
